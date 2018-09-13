00131e51b4f643670ef02af1953842d0a45b6319
zeeps13 <stephen.brady@bmlg.net>
2018-09-13 15:33:38 -0500

    Add unittest.main, correct file reference

--- hackathon_tests.py
+++ hackathon_tests.py
@@ -10,7 +10,7 @@ from resource_monitor import ResourceManager
 class TestConnectedComponents(unittest.TestCase):
     def test_connected_components(self):
 
-        mythread = ResourceManager(connected_components, 'hackathon_dataset_25.csv')
+        mythread = ResourceManager(connected_components, 'input.csv')
         mythread.start()
 
         start_mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
@@ -33,3 +33,7 @@ class TestConnectedComponents(unittest.TestCase):
         data = pd.read_csv('output.csv')
         # Expected number of unique cluster_ids
         self.assertEqual(len(data.cluster_id.value_counts()), 20316767)
+
+
+if __name__ == '__main__':
+    unittest.main()
